ec0586c8420320ee09bd743dc68acd2cdc6e7adc
ElaineTavares <etsets.tavares@gmail.com>
2023-09-14 18:03:00 -0300

    java

---
+++ public/js/abas.js
@@ -0,0 +1,9 @@
+function showConsultoria() {
+    document.getElementById('profissionais-consultoria').style.display = 'block'
+    document.getElementById('profissionais-servico').style.display = 'none'
+}
+
+function showServico() {
+    document.getElementById('profissionais-consultoria').style.display = 'none'
+    document.getElementById('profissionais-servico').style.display = 'block'
+}
---
+++ public/js/menu.js
@@ -0,0 +1,7 @@
+function changeMenu() {
+    if (document.getElementById('menu').style.display == 'none') {
+        document.getElementById('menu').style.display = 'flex'
+    } else {
+        document.getElementById('menu').style.display = 'none'
+    }
+}
